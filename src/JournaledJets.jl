0b867187e053bbfd25e3d80edf0a267e931b0727
tqff <sam.kaplan@chevron.com>
2020-01-07 16:20:42 +0000

    WIP -- before fixing communication with master block futures

--- src/JournaledJets.jl
+++ src/JournaledJets.jl
@@ -1,67 +1,118 @@
 module JournaledJets
 
-using Distributed, Jets, LinearAlgebra, Schedulers
+using Distributed, Jets, LinearAlgebra, ParallelOperations, Schedulers
 
-struct DJArray{T,A<:AbstractArray{T,1}} <: AbstractArray{T,1}
-    blocks::Vector{Future}
-    indices::Vector{UnitRange{Int}}
+struct DJArray{T,N,A<:AbstractArray{T}} <: AbstractArray{T,N}
+    blocks::Array{Future,N}
+    indices::Array{NTuple{N,UnitRange{Int}},N}
     journal::Vector{Expr}
 end
 
 DJArray_construct_block(iblock, f) = remotecall(f, myid(), iblock)
 DJArray_block_length(block) = length(fetch(block))
+DJArray_block_size(block, i) = size(fetch(block), i)
 DJArray_block_typeof(block) = typeof(fetch(block))
 
-function DJArray(f::Function, nblocks::Integer, pids)
-    blocks = cvxpmap(DJArray_construct_block, 1:nblocks, f)
+function DJArray(f::Function, nblocks::NTuple{1,Int}, pids)
+    blocks = cvxpmap(Future, DJArray_construct_block, 1:nblocks[1], f)
 
-    indices = Vector{UnitRange{Int}}(undef, nblocks)
+    indices = Vector{NTuple{1,UnitRange{Int}}}(undef, nblocks)
     i1 = 1
     for (iblock,block) in enumerate(blocks)
         i2 = i1 + remotecall_fetch(DJArray_block_length, block.where, block) - 1
-        indices[iblock] = i1:i2
+        indices[iblock] = (i1:i2,)
         i1 = i2 + 1
     end
 
     A = remotecall_fetch(DJArray_block_typeof, blocks[1].where, blocks[1])
     T = eltype(A)
 
-    DJArray{T,A}(blocks, indices, Expr[])
+    DJArray{T,1,A}(blocks, indices, Expr[])
+end
+
+function DJArray(f::Function, nblock::NTuple{N,Int}, pids) where {N}
+    blocks = cvxpmap(Future, DJArray_construct_block, CartesianIndices(nblock), f)
+
+    i1 = [Int[] for i=1:N]
+    i2 = [Int[] for i=1:N]
+    for idim = 1:N
+        _i1 = 1
+        for iblock = 1:nblock[idim]
+            block = blocks[ntuple(_idim->_idim==idim ? iblock : 1, N)...]
+            _i2 = _i1 + remotecall_fetch(DJArray_block_size, block.where, block, idim) - 1
+            push!(i1[idim], _i1)
+            push!(i2[idim], _i2)
+            _i1 = _i2 + 1
+        end
+    end
+
+    indices = Array{NTuple{N,UnitRange{Int}},N}(undef, nblock)
+    for (i,idx) in enumerate(CartesianIndices(nblock))
+        indices[idx] = ntuple(idim->i1[idim][idx[idim]]:i2[idim][idx[idim]], N)
+    end
+
+    A = remotecall_fetch(DJArray_block_typeof, blocks[1].where, blocks[1])
+    T = eltype(A)
+
+    DJArray{T,N,A}(blocks, indices, Expr[])
 end
 
 # DJArray interface implementation <--
-Base.IndexStyle(::Type{T}) where {T<:DJArray} = IndexLinear()
-Base.size(x::DJArray) = (x.indices[end][end],)
-Jets.indices(x::DJArray) = x.indices
-Jets.getblock(x::DJArray{T,A}, iblock::Int) where {T,A} = fetch(x.blocks[iblock])::A
-DJArray_setblock_local!(block, xblock, ::Type{A}) where {A} = fetch(block)::A .= xblock
-Jets.setblock!(x::DJArray{T,A}, iblock::Int, xblock) where {T,A} = remotecall_fetch(DJArray_setblock_local!, blocks(x)[iblock].where, blocks(x)[iblock], xblock, A)
-blocks(x::DJArray) = x.blocks
-nblocks(x::DJArray) = length(blocks(x))
+function _getblock(x::DJArray{T,N,A}, iblock::Int) where {T,N,A}
+    xblock = fetch(blocks(x)[iblock])::A
+    _where = blocks(x)[iblock].where
+    _myid = myid()
+    if _myid != _where
+        x.blocks[iblock] = remotecall(identity, _myid, xblock)
+        # send the future back to the master pid .. how do we do this ??
+        # do we need x to contain some reference to the master-held blocks (i.e. a Future)
+        @info "_myid=$_myid, iblock=$iblock, where=$(blocks(x)[iblock].where)"
+        @info "before fetch, ex=$(extrema(xblock))"
+        xblock = fetch(blocks(x)[iblock])
+        @info "after fetch, ex=$(extrema(xblock))"
+        @debug "Owner of block $iblock has switched from process $_where to $_myid, eltype(xblock)=$(eltype(xblock))"
+    end
+    xblock
+end
 
-DJArray_getindex(::Type{A}, block_future, j) where {A} = getindex(fetch(block_future)::A, j)
-function Base.getindex(x::DJArray{T,A}, i::Int) where {T,A}
-    iblock = findfirst(rng->i∈rng, indices(x))
+blocks(x::DJArray) = x.blocks
+Jets.indices(x::DJArray) = x.indices
+size_dim(x::DJArray{T,N}, idim) where {T,N} = x.indices[ntuple(_idim->_idim==idim ? size(x.indices)[idim] : 1, N)...][idim][end]
+Base.size(x::DJArray{T,N}) where {T,N} = ntuple(idim->size_dim(x, idim), N)
+Jets.getblock(x::DJArray, iblock::Int...) = _getblock(x, LinearIndices(size(blocks(x)))[iblock...])
+Jets.getblock(x::DJArray, iblock::CartesianIndex) = getblock(x, iblock.I...)
+DJArray_setblock_local!(block, xblock, ::Type{A}) where {A} = fetch(block)::A .= xblock
+_setblock!(x::DJArray{T,N,A}, xblock, iblock::Int) where {T,N,A} = remotecall_fetch(DJArray_setblock_local!, blocks(x)[iblock].where, blocks(x)[iblock], xblock, A)
+Jets.setblock!(x::DJArray, xblock, iblock::Int...) = _setblock!(x, xblock, LinearIndices(size(blocks(x)))[iblock...])
+Jets.setblock!(x::DJArray, xblock, iblock::CartesianIndex) = setblock!(x, xblock, iblock.I...)
+size_blocks(x::DJArray) = size(blocks(x))
+length_blocks(x::DJArray) = prod(size(blocks(x)))
+Jets.nblocks(x::DJArray) = length_blocks(x)
+
+DJArray_getindex(::Type{A}, block_future, δi) where {A} = getindex(fetch(block_future)::A, δi...)
+function Base.getindex(x::DJArray{T,N,A}, i::Vararg{Int,N}) where {T,N,A}
+    iblock = findfirst(rng->mapreduce(idim->i[idim]∈rng[idim], &, 1:N), indices(x))
     block_future = x.blocks[iblock]
-    iₒ = indices(x)[iblock][1]
-    remotecall_fetch(DJArray_getindex, block_future.where, A, block_future, i-iₒ+1)
+    iₒ = ntuple(idim->indices(x)[iblock][idim][1], N)
+    δi = ntuple(idim->i[idim]-iₒ[idim]+1, N)
+    remotecall_fetch(DJArray_getindex, block_future.where, A, block_future, δi)
 end
 
 _DJArray_similar_block(iblock, x::DJArray, ::Type{S}) where {S} = similar(getblock(x, iblock), S)
 DJArray_similar_block(iblock, x::DJArray, ::Type{S}) where {S} = remotecall(_DJArray_similar_block, myid(), iblock, x, S)
 DJArray_similar_typeof(block) = typeof(fetch(block))
-function Base.similar(x::DJArray{T,A}, ::Type{S}) where {T,A,S}
+function Base.similar(x::DJArray{T,N,A}, ::Type{S}) where {T,N,A,S}
     _indices = copy(indices(x))
-    _block_futures = cvxpmap(DJArray_similar_block, 1:nblocks(x), x, S)
+    _block_futures = cvxpmap(DJArray_similar_block, CartesianIndices(size_blocks(x)), x, S)
     _A = remotecall_fetch(DJArray_similar_typeof, _block_futures[1].where, _block_futures[1])
-    DJArray{S,_A}(_block_futures, _indices, Expr[])
+    DJArray{S,N,_A}(_block_futures, _indices, Expr[])
 end
 
 DJArray_local_norm(iblock, x::DJArray, p) = norm(getblock(x, iblock), p)
 
 function LinearAlgebra.norm(x::DJArray{T}, p::Real=2) where {T}
     _T = float(real(T))
-    z = cvxpmap(DJArray_local_norm, 1:nblocks(x), x, p)
+    z = cvxpmap(DJArray_local_norm, 1:length_blocks(x), x, p)
     if p == Inf
         maximum(z)
     elseif p == -Inf
@@ -77,13 +128,13 @@ end
 DJArray_local_dot(iblock, x, y) = dot(getblock(x, iblock), getblock(y, iblock))
 
 function LinearAlgebra.dot(x::DJArray, y::DJArray)
-    z = cvxpmap(DJArray_local_dot, 1:nblocks(x), x, y)
+    z = cvxpmap(DJArray_local_dot, 1:length_blocks(x), x, y)
     sum(z)
 end
 
 DJArray_local_extrema(iblock, x::DJArray) = extrema(getblock(x, iblock))
 function Base.extrema(x::DJArray{T}) where {T}
-    mnmx = cvxpmap(DJArray_local_extrema, 1:nblocks(x), x)
+    mnmx = cvxpmap(DJArray_local_extrema, 1:length_blocks(x), x)
     mn, mx = mnmx[1]
     for i = 2:length(mnmx)
         _mn, _mx = extrema(mnmx[i])
@@ -92,6 +143,7 @@ function Base.extrema(x::DJArray{T}) where {T}
     end
     mn,mx
 end
+
 DJArray_local_fill!(block, a) = fill!(fetch(block), a)
 function Base.fill!(x::DJArray, a)
     for block in blocks(x)
@@ -101,17 +153,18 @@ end
 
 Distributed.procs(x::DJArray) = unique([block.where for block in blocks(x)])
 
-function Base.collect(x::DJArray{T,A}) where {T,A}
-    _x = A[]
+function Base.collect(x::DJArray{T,1,A}) where {T,A}
+    _x = Vector{A}(undef, length_blocks(x))
     n = 0
-    for iblock = 1:nblocks(x)
-        push!(_x, getblock(x, iblock))
+    for iblock = CartesianIndices(size_blocks(x))
+        _x[iblock] = fetch(x.blocks[iblock])
     end
-    Jets.BlockArray(_x, indices(x))
+    indicesx = indices(x)
+    Jets.BlockArray(_x, [indicesx[i][1] for i=1:length(indicesx)])
 end
 
-Base.convert(::Type{T}, x::DJArray) where {T<:Jets.BlockArray} = collect(x)
-Base.convert(::Array, x::DJArray) = convert(Array, collect(x))
+Base.convert(::Type{S}, x::DJArray{T,1,A}) where {S<:Jets.BlockArray,T,A} = collect(x)
+Base.convert(::Array, x::DJArray{T,1,A}) where {T,A} = convert(Array, collect(x))
 # -->
 
 # DJArray broadcasting implementation --<
@@ -129,12 +182,12 @@ find_djarray(::Any, rest) = find_djarray(rest)
 Jets.getblock(A::DJArray, ::Type{<:Any}, iblock, indices) = getblock(A, iblock)
 
 function DJArray_bcast_local_copyto!(iblock, dest, bc, S)
-    setblock!(dest, iblock, getblock(bc, S, iblock, indices(dest)[iblock]))
+    setblock!(dest, getblock(bc, S, iblock, indices(dest)[iblock][1]), iblock)
     nothing
 end
-function Base.copyto!(dest::DJArray{T,<:AbstractArray{T,N}}, bc::Broadcast.Broadcasted{DJArrayStyle}) where {T,N}
+function Base.copyto!(dest::DJArray{T,1,<:AbstractArray{T,N}}, bc::Broadcast.Broadcasted{DJArrayStyle}) where {T,N}
     S = Broadcast.DefaultArrayStyle{N}
-    cvxpmap(DJArray_bcast_local_copyto!, 1:nblocks(dest), dest, bc, S)
+    cvxpmap(DJArray_bcast_local_copyto!, 1:length_blocks(dest), dest, bc, S)
     dest
 end
 # -->
@@ -143,14 +196,14 @@ end
 # JetJSpace
 #
 struct JetJSpace{T,S<:Jets.JetAbstractSpace{T}} <: Jets.JetAbstractSpace{T,1}
-    spaces::DJArray{S,Array{S,1}}
+    spaces::DJArray{S,1,Array{S,1}}
     indices::Vector{UnitRange{Int}}
 end
 
 JetJSpace_length(iblock, spaces) = length(getblock(spaces, iblock)[1])
 
-function JetJSpace(spaces::DJArray{S,A}) where {S<:JetAbstractSpace, A<:AbstractArray{S}}
-    n = cvxpmap(JetJSpace_length, 1:nblocks(spaces), spaces)
+function JetJSpace(spaces::DJArray{S,1,A}) where {S<:JetAbstractSpace, A<:AbstractArray{S}}
+    n = cvxpmap(JetJSpace_length, 1:length_blocks(spaces), spaces)
     iₒ = 1
     indices = Vector{UnitRange{Int}}(undef, length(spaces))
     for (iblock,block) in enumerate(blocks(spaces))
@@ -168,13 +221,119 @@ Base.eltype(R::Type{JetJSpace{T,S}}) where {T,S} = T
 Jets.indices(R::JetJSpace) = R.indices
 Jets.space(R::JetJSpace, iblock::Integer) = getblock(R.spaces, iblock)[1]
 Jets.nblocks(R::JetJSpace) = length(R.spaces)
+Distributed.procs(R::JetJSpace) = procs(R.spaces)
+
+for f in (:Array, :ones, :rand, :zeros)
+    @eval (Base.$f)(R::JetJSpace) = DJArray(iblock->($f)(space(R, iblock)), (nblocks(R),), procs(R))
+end
 
 #
 # Block operator
 #
+JetJBlock_dom(iblock, ops) = domain(getblock(ops, 1, iblock)[1])
+JetJBlock_rng(iblock, ops) = range(getblock(ops, iblock, 1)[1])
+JetJBlock_spc_length(iblock, blkspaces) = length(getblock(blkspaces, iblock)[1])
 function Jets.JetBlock(ops::DJArray{T,2}) where {T<:Jop}
-    n1,n2 = length(indices(ops, 1)),length(indices(ops,2))
-    Jet(dom = dom, rng = rng, f! = JetJBlock_f!, df! = JetJBlock_df!, df′! = JetJBlock_df′!, s = (ops=_ops, dom=dom, rng=rng))
+    n1,n2 = size(ops)
+
+    local dom
+    if n2 == 1
+        dom = remotecall_fetch(JetJBlock_dom, blocks(ops)[1,1].where, 1, ops)
+    else
+        indices_dom = Vector{UnitRange{Int}}(undef, n2)
+        blkspaces_dom = DJArray(iblock->[JetJBlock_dom(iblock, ops)], (n2,), workers())
+        n = cvxpmap(Int, JetJBlock_spc_length, 1:n2, blkspaces_dom)
+        i1 = 1
+        for i = 1:length(n)
+            i2 = i1 + n[i] - 1
+            indices_dom[i] = i1:i2
+            i1 = i2 + 1
+        end
+        dom = JetJSpace(blkspaces_dom, indices_dom)
+    end
+
+    local rng
+    if n1 == 1
+        rng = remotecall_fetch(JetDJBlock_rng, block(ops)[1,1].where, 1, ops)
+    else
+        indices_rng = Vector{UnitRange{Int}}(undef, n1)
+        blkspaces_rng = DJArray(iblock->[JetJBlock_rng(iblock, ops)], (n1,), workers())
+        n = cvxpmap(Int, JetJBlock_spc_length, 1:n1, blkspaces_rng)
+        i1 = 1
+        for i = 1:length(n)
+            i2 = i1 + n[i] - 1
+            indices_rng[i] = i1:i2
+            i1 = i2 + 1
+        end
+        rng = JetJSpace(blkspaces_rng, indices_rng)
+    end
+    Jet(dom = dom, rng = rng, f! = JetJBlock_f!, df! = JetJBlock_df!, df′! = JetJBlock_df′!, s = (ops=ops, dom=dom, rng=rng))
+end
+
+function addmasterpid(pids)
+    if myid() ∉ pids
+        return [myid();pids]
+    end
+    pids
+end
+
+function JetJBlock_local_f!(iblock, d, _m, ops)
+    @info "^^^^^before getblock, iblock=$iblock, d.blocks[4].where=$(d.blocks[4].where), myid()=$(myid())"
+    dᵢ = getblock(d, iblock)
+    @info ">>>>>after getblock, iblock=$iblock, d.blocks[4].where=$(d.blocks[4].where), myid()=$(myid())"
+    opsᵢ = getblock(ops, iblock)[1]
+    m = localpart(_m)
+    @info "***before, iblock=$iblock, extrema(dᵢ)=$(extrema(dᵢ)), myid()=$(myid())"
+    mul!(dᵢ, opsᵢ, m)
+    @info "---after, iblock=$iblock, extrema(dᵢ)=$(extrema(dᵢ)), myid()=$(myid())"
+    nothing
+end
+
+function JetJBlock_f!(d::DJArray, m::AbstractArray; ops, dom, rng)
+    pids = procs(d)
+    _m = bcast(m, addmasterpid(pids))
+    cvxpmap(JetJBlock_local_f!, 1:nblocks(d), d, _m, ops)
+    d
+end
+
+function JetJBlock_local_df!(iblock, d, _m, ops)
+    dᵢ = getblock(d, iblock)
+    opsᵢ = getblock(ops, iblock)[1]
+    m = localpart(_m)
+    mul!(dᵢ, JopLn(opsᵢ), m)
+    nothing
+end
+
+function JetJBlock_df!(d::DJArray, m::AbstractArray; ops, kwargs...)
+    pids = procs(d)
+    _m = bcast(m, addmasterpid(pids))
+    cvxpmap(JetJBlock_local_df!, 1:nblocks(d), d, _m, ops)
+    d
+end
+
+function JetJBlock_local_df′!(iblock, _m, d, ops)
+    m = localpart(_m)
+    dᵢ = getblock(d, iblock)
+    opsᵢ = JopLn(getblock(ops, iblock)[1])
+    m .+= opsᵢ' * dᵢ
+    nothing
+end
+
+function JetJBlock_df′!(m::AbstractArray, d::DJArray; ops, kwargs...)
+    pids = procs(d)
+    m .= 0
+    cvxpmapreduce!(m, JetJBlock_local_df′!, 1:nblocks(d), (d, ops))
+end
+
+function JetJBlock_local_point!(i, j, mₒ)
+    op = getblock(state(j).ops, i)[1]
+    Jets.point!(jet(op), getblock(mₒ, i))
+    nothing
+end
+
+function Jets.point!(jet::Jet{D,R,typeof(JetJBlock_f!)}, mₒ::AbstractArray) where {D<:Jets.JetAbstractSpace, R<:Jets.JetAbstractSpace}
+    cvxpmap(JetJBlock_local_point!, CartesianIndices(size(state(jet).ops)), jet, mₒ)
+    jet
 end
 
 export DJArray, JetJSpace
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,33 @@
 using Revise
-using Distributed, Jets, JournaledJets, LinearAlgebra, Schedulers, Test
+using Distributed, JetPack, Jets, JournaledJets, LinearAlgebra, Schedulers, Test
 
 foo(iblock) = iblock * ones(2)
+bar(iblock) = LinearIndices((3,2))[iblock[1],iblock[2]] * ones(2,3)
 
-@testset "DJArray construct" begin
-    x = DJArray(foo, 3, workers())
-    @test typeof(x) == DJArray{Float64, Vector{Float64}}
+@testset "DJArray construct, 1D" begin
+    x = DJArray(foo, (3,), workers())
+    @test typeof(x) == DJArray{Float64, 1, Vector{Float64}}
 end
 
-@testset "DJArray size" begin
-    x = DJArray(foo, 3, workers())
+@testset "DJArray construct, 2D" begin
+    x = DJArray(bar, (3,2), workers())
+    @test typeof(x) == DJArray{Float64, 2, Array{Float64,2}}
+end
+
+@testset "DJArray size, 1D" begin
+    x = DJArray(foo, (3,), workers())
     @test size(x) == (6,)
     @test length(x) == 6
 end
 
-@testset "DJArray getblock" begin
-    x = DJArray(foo, 3, workers())
+@testset "DJArray size, 2D" begin
+    x = DJArray(bar, (3,2), workers())
+    @test size(x) == (6,6)
+    @test length(x) == 36
+end
+
+@testset "DJArray getblock, 1D" begin
+    x = DJArray(foo, (3,), workers())
     x₁ = getblock(x, 3)
 
     for iblock = 1:3
@@ -24,17 +36,36 @@ end
     end
 end
 
-@testset "DJArray setblock!" begin
-    x = DJArray(foo, 3, workers())
+@testset "DJArray getblock, 2D" begin
+    x = DJArray(bar, (3,2), workers())
+    x₁₁ = getblock(x, 1, 1)
+
+    for jblock = 1:2, iblock = 1:3
+        xᵢⱼ = getblock(x, iblock, jblock)
+        @test xᵢⱼ ≈ LinearIndices((3,2))[iblock,jblock]*ones(2,3)
+    end
+end
+
+@testset "DJArray setblock!, 1D" begin
+    x = DJArray(foo, (3,), workers())
     for iblock = 1:3
-        setblock!(x, iblock, 2*iblock*ones(2))
+        setblock!(x, 2*iblock*ones(2), iblock)
         xᵢ = getblock(x, iblock)
         @test xᵢ ≈ 2*iblock*ones(2)
     end
 end
 
-@testset "DJArray getindex" begin
-    x = DJArray(foo, 3, workers())
+@testset "DJArray setblock!, 2D" begin
+    x = DJArray(bar, (3,2), workers())
+    for jblock = 1:2, iblock = 1:3
+        setblock!(x, 2*LinearIndices((3,2))[iblock,jblock]*ones(2,3), iblock, jblock)
+        xᵢⱼ = getblock(x, iblock, jblock)
+        @test xᵢⱼ ≈ 2*LinearIndices((3,2))[iblock,jblock]*ones(2,3)
+    end
+end
+
+@testset "DJArray getindex, 1D" begin
+    x = DJArray(foo, (3,), workers())
     for i = 1:length(x)
         if i <= 2
             @test x[i] ≈ 1
@@ -46,10 +77,29 @@ end
     end
 end
 
-@testset "DJArray similar" begin
+@testset "DJArray getindex, 2D" begin
+    x = DJArray(bar, (3,2), workers())
+    for j = 1:size(x,2), i = 1:size(x,1)
+        if 1 <= i <= 2 && 1 <= j <= 3
+            @test x[i,j] ≈ 1
+        elseif 3 <= i <= 4 && 1 <= j <= 3
+            @test x[i,j] ≈ 2
+        elseif 5 <= i <= 6 && 1 <= j <= 3
+            @test x[i,j] ≈ 3
+        elseif 1 <= i <= 2 && 4 <= j <= 6
+            @test x[i,j] ≈ 4
+        elseif 3 <= i <= 4 && 4 <= j <= 6
+            @test x[i,j] ≈ 5
+        elseif 5 <= i <= 6 && 4 <= j <= 6
+            @test x[i,j] ≈ 6
+        end
+    end
+end
+
+@testset "DJArray similar, 1D" begin
     addprocs(3)
     @everywhere using JournaledJets, Jets
-    x = DJArray(i->rand(2), 3, workers())
+    x = DJArray(i->rand(2), (3,), workers())
     y = similar(x)
     z = similar(x, Float32)
     @test length(x) == length(y)
@@ -60,18 +110,34 @@ end
     rmprocs(workers())
 end
 
+@testset "DJArray similar, 2D" begin
+    addprocs(3)
+    @everywhere using JournaledJets, Jets
+    x = DJArray(i->rand(2,3), (3,2), workers())
+    y = similar(x)
+    z = similar(x, Float32)
+    @test length(x) == length(y)
+    @test length(z) == length(y)
+    @test size(x) == size(y)
+    @test size(z) == size(y)
+    @test eltype(z) == Float32
+    @test indices(z) == indices(x)
+    @test indices(y) == indices(x)
+    rmprocs(workers())
+end
+
 @testset "DJArray procs" begin
     addprocs(3)
     @everywhere using JournaledJets
-    x = DJArray(i->rand(2), 3, workers());
+    x = DJArray(i->rand(2), (3,), workers());
     @test procs(x) == workers()
     rmprocs(workers())
 end
 
-@testset "DJArray collect/convert" begin
+@testset "DJArray collect/convert, 1D" begin
     addprocs(3)
     @everywhere using JournaledJets
-    x = DJArray(i->rand(2), 3, workers())
+    x = DJArray(i->rand(2), (3,), workers())
     y = collect(x)
     z = convert(Jets.BlockArray, x)
     u = convert(Array, x)
@@ -87,11 +153,11 @@ end
     rmprocs(workers())
 end
 
-@testset "DJArray norm" begin
+@testset "DJArray norm, 1D" begin
     addprocs(3)
     @everywhere using Jets, JournaledJets, LinearAlgebra
     x = rand(90)
-    y = DJArray(i->x[(i-1)*30+1:i*30], 3, workers())
+    y = DJArray(i->x[(i-1)*30+1:i*30], (3,), workers())
     z = collect(y)
     @test x ≈ z
 
@@ -107,8 +173,8 @@ end
     @everywhere using JournaledJets, LinearAlgebra
     x₁ = rand(90)
     x₂ = rand(90)
-    y₁ = DJArray(i->x₁[(i-1)*30+1:i*30], 3, workers())
-    y₂ = DJArray(i->x₂[(i-1)*30+1:i*30], 3, workers())
+    y₁ = DJArray(i->x₁[(i-1)*30+1:i*30], (3,), workers())
+    y₂ = DJArray(i->x₂[(i-1)*30+1:i*30], (3,), workers())
     z₁ = collect(y₁)
     z₂ = collect(y₂)
     @test x₁ ≈ z₁
@@ -121,19 +187,19 @@ end
     addprocs(3)
     @everywhere using JournaledJets, LinearAlgebra
     x = rand(90)
-    y = DJArray(i->x[(i-1)*30+1:i*30], 3, workers())
+    y = DJArray(i->x[(i-1)*30+1:i*30], (3,), workers())
     z = collect(y)
-    @test x ≈ y
     mnmx = extrema(y)
     _mnmx = extrema(x)
     @test mnmx[1] ≈ _mnmx[1]
     @test mnmx[2] ≈ _mnmx[2]
+    rmprocs(workers())
 end
 
 @testset "DJArray fill!" begin
     addprocs(3)
     @everywhere using JournaledJets, LinearAlgebra
-    y = DJArray(i->zeros(30), 3, workers())
+    y = DJArray(i->zeros(30), (3,), workers())
     y .= 3
     z = collect(y)
     for i = 1:90
@@ -145,23 +211,23 @@ end
 @testset "DJArray broadcasting" begin
     addprocs(3)
     @everywhere using Jets, JournaledJets
-    u = DJArray(i->rand(10), 4, workers())
-    v = DJArray(i->rand(10), 4, workers())
-    w = DJArray(i->rand(10), 4, workers())
+    u = DJArray(i->rand(10), (4,), workers())
+    v = DJArray(i->rand(10), (4,), workers())
+    w = DJArray(i->rand(10), (4,), workers())
     a = rand(Float64)
     b = rand(Float64)
     c = rand(Float64)
     x = a*u .+ b*v .+ c*w
-    @test typeof(x) == JournaledJets.DJArray{Float64,Array{Float64,1}}
+    @test typeof(x) == JournaledJets.DJArray{Float64,1,Array{Float64,1}}
 
     _u = collect(u)
     _v = collect(v)
     _w = collect(w)
     @test a*_u .+ b*_v .+ c*_w ≈ collect(x)
 
-    y = DJArray(i->zeros(10), 4, workers())
+    y = DJArray(i->zeros(10), (4,), workers())
     y .= x
-    @test typeof(y) == JournaledJets.DJArray{Float64,Array{Float64,1}}
+    @test typeof(y) == JournaledJets.DJArray{Float64,1,Array{Float64,1}}
     @test a*_u .+ b*_v .+ c*_w ≈ collect(y)
 
     z = zeros(40)
@@ -169,7 +235,7 @@ end
     @test a*_u .+ b*_v .+ c*_w ≈ z
 
     x = rand(Int32, 40)
-    y = DJArray(i->rand(10), 4, workers())
+    y = DJArray(i->rand(10), (4,), workers())
     z = x .* y
     @test isa(z, JournaledJets.DJArray)
     @test convert(Array, z) ≈ x .* convert(Array, y)
@@ -183,7 +249,7 @@ end
     addprocs(3)
     workers()
     @everywhere using Jets, JournaledJets
-    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], 5, workers()))
+    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], (5,), workers()))
     @test isa(R, JetJSpace)
     rmprocs(workers())
 end
@@ -191,7 +257,7 @@ end
 @testset "JetJSpace size" begin
     addprocs(3)
     @everywhere using Jets, JournaledJets
-    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], 5, workers()))
+    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], (5,), workers()))
     size(R)
     @test size(R) == (50,)
     rmprocs(workers())
@@ -201,7 +267,7 @@ end
     addprocs(3)
     workers()
     @everywhere using Jets, JournaledJets
-    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], 5, workers()))
+    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], (5,), workers()))
     @test eltype(R) == Float64
     rmprocs(workers())
 end
@@ -209,7 +275,7 @@ end
 @testset "JetJSpace indices" begin
     addprocs(3)
     @everywhere using Jets, JournaledJets
-    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], 5, workers()))
+    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], (5,), workers()))
     @test indices(R) == [1:10, 11:20, 21:30, 31:40, 41:50]
     rmprocs(workers())
 end
@@ -217,7 +283,7 @@ end
 @testset "JetJSpace space" begin
     addprocs(3)
     @everywhere using Jets, JournaledJets
-    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], 5, workers()))
+    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], (5,), workers()))
     space(R,2)
     @test space(R, 2) == JetSpace(Float64,10)
     rmprocs(workers())
@@ -226,7 +292,99 @@ end
 @testset "JetJSpace nblocks" begin
     addprocs(3)
     @everywhere using Jets, JournaledJets
-    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], 5, workers()))
+    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], (5,), workers()))
     @test nblocks(R) == 5
     rmprocs(workers())
 end
+
+@testset "JetJSpace Array" begin
+    addprocs(3)
+    @everywhere using Jets, JournaledJets
+    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], (5,), workers()))
+    x = Array(R)
+    @test isa(x, DJArray)
+    @test size(x) == (50,)
+    rmprocs(workers())
+end
+
+@testset "JetJSpace ones" begin
+    addprocs(3)
+    @everywhere using Jets, JournaledJets
+    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], (5,), workers()))
+    x = ones(R)
+    @test isa(x, DJArray)
+    @test size(x) == (50,)
+    @test collect(x) ≈ ones(50)
+    rmprocs(workers())
+end
+
+@testset "JetJSpace zeros" begin
+    addprocs(3)
+    @everywhere using Jets, JournaledJets
+    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], (5,), workers()))
+    x = zeros(R)
+    @test isa(x, DJArray)
+    @test size(x) == (50,)
+    @test collect(x) ≈ zeros(50)
+    rmprocs(workers())
+end
+
+@testset "JetJSpace rand" begin
+    addprocs(3)
+    @everywhere using Jets, JournaledJets
+    R = JetJSpace(DJArray(_->[JetSpace(Float64,10)], (5,), workers()))
+    x = rand(R)
+    @test isa(x, DJArray)
+    @test size(x) == (50,)
+    rmprocs(workers())
+end
+
+@testset "block operator, tall and skinny" begin
+    A = @blockop DJArray(i->[JopDiagonal(rand(2))], (2,3), workers())
+    @test isa(A, Jop)
+end
+
+#@testset "JopJBlock, homogeneous, tall and skinny" begin
+using Distributed
+addprocs(3)
+@everywhere using Jets, JournaledJets, Logging, Test, LinearAlgebra
+
+@everywhere Logging.global_logger(Logging.ConsoleLogger(stdout, Logging.Debug))
+
+@everywhere JopBar_f!(d,m) = d .= m.^2
+@everywhere JopBar_df!(δd,δm;mₒ,kwargs...) = δd .= 2 .* mₒ .* δm
+@everywhere function JopBar(n) spc = JetSpace(Float64, n)
+    JopNl(f! = JopBar_f!, df! = JopBar_df!, df′! = JopBar_df!, dom = spc, rng = spc)
+end
+
+_F  = DJArray(_->[JopBar(10)], (4,1), workers())
+F = @blockop _F
+
+_G = [_F[i] for i in 1:4, j in 1:1]
+G = @blockop _G
+@test isa(F, JopNl{<:Jet{<:JetSpace,<:JetJSpace,typeof(JournaledJets.JetJBlock_f!)}})
+
+m = rand(domain(F))
+d = F*m;
+_d = G*m;
+__d = collect(d)
+
+@test _d ≈ __d
+
+JournaledJets.blocks(d)[4].where
+
+@test collect(F*m) ≈ G*m
+
+F*m
+
+J = jacobian!(F, m)
+_J = jacobian!(G, m)
+
+δm = rand(domain(J))
+@test collect(J*δm) ≈ _J*δm
+
+δd = rand(range(J))
+@test J'*δd ≈ _J'*collect(δd)
+
+rmprocs(workers())
+#end
